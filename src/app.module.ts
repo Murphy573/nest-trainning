4660d4efaee42a8ac3519bcb163eb3ac9d29c352
Murphy573 <429655198@qq.com>
2024-06-06 18:02:59 +0800

    feat: 修改全局守卫使用的位置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,32 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnimalModule } from './modules/animal/animal.module';
 import { PersonModule } from './modules/person/person.module';
 import { StudentModule } from './modules/student/student.module';
 import { GlobalModule } from './modules/common/global.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [AnimalModule, PersonModule, StudentModule, GlobalModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // 全局pipe
+    {
+      provide: APP_PIPE,
+      useFactory: () => {
+        return new ValidationPipe({
+          enableDebugMessages: true,
+          forbidNonWhitelisted: true,
+          whitelist: true,
+          transform: true,
+          transformOptions: {
+            enableImplicitConversion: true,
+          },
+        });
+      },
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      enableDebugMessages: true,
-      forbidNonWhitelisted: true,
-      whitelist: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
   await app.listen(3000);
 }
 bootstrap();
